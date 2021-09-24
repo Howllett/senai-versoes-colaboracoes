53dd2beb3ed5822cb595aa303a026e65f146343e
Howllett <90012994+Howllett@users.noreply.github.com>
2021-09-23 21:27:30 -0300

    Add files via upload

---
+++ clasificacao.js
@@ -0,0 +1,14 @@
+var quantidadedealunos = 14;
+
+for (var controle = 0;controle < quantidadedealunos;controle++){
+    if(controle == 0){
+        console.log("o numero é zero")
+    }
+    else if(controle% 2 == 0){
+        console.log("o numero " + controle + " é par")
+         
+
+    }else {
+        console.log("o numero " + controle + "é impar")
+    }
+}
